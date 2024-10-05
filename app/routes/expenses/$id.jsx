6e175a242adb9559e03b71516d7e4f99af4ac46b
Andrii Vakhotskyi <andrey.vahotskiy@devit.group>
2024-10-05 13:40:02 +0300

    Rendering a Modal Page (Overlay Page)

--- app/routes/expenses/$id.jsx
+++ app/routes/expenses/$id.jsx
@@ -1,5 +1,18 @@
+import { useNavigate } from "@remix-run/react";
+
 import ExpenseForm from "../../components/expenses/ExpenseForm";
+import Modal from "../../components/util/Modal";
 
 export default function ExpensesItemPage() {
-  return <ExpenseForm />;
+  const navigate = useNavigate();
+
+  const closeHandler = () => {
+    navigate("..");
+  };
+
+  return (
+    <Modal onClose={closeHandler}>
+      <ExpenseForm />
+    </Modal>
+  );
 }
--- app/routes/expenses/add.jsx
+++ app/routes/expenses/add.jsx
@@ -1,5 +1,19 @@
+import { useNavigate } from "@remix-run/react";
+
 import ExpenseForm from "../../components/expenses/ExpenseForm";
 
+import Modal from "../../components/util/Modal";
+
 export default function AddExpensesPage() {
-  return <ExpenseForm />;
+  const navigate = useNavigate();
+
+  const closeHandler = () => {
+    navigate("..");
+  };
+
+  return (
+    <Modal onClose={closeHandler}>
+      <ExpenseForm />
+    </Modal>
+  );
 }
