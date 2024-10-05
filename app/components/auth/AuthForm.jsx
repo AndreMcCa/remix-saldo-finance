4cb45c586fa4078dfdde3468d0242740516f7201
Andrii Vakhotskyi <andrey.vahotskiy@devit.group>
2024-10-05 20:46:59 +0300

    refactor:  authentication form component

--- app/components/auth/AuthForm.jsx
+++ app/components/auth/AuthForm.jsx
@@ -1,11 +1,19 @@
-import { FaLock } from 'react-icons/fa';
+import { Link, useSearchParams } from "@remix-run/react";
+import { FaLock, FaUserPlus } from "react-icons/fa";
 
 function AuthForm() {
+  const [searchParams] = useSearchParams();
+  const authMode = searchParams.get("mode") || "login";
+  const isLoginMode = authMode === "login";
+
+  const submitBtnCaption = isLoginMode ? "Login" : "Create Account";
+  const toggleBtnCaption = isLoginMode ? "Create new account" : "Log in with existing user";
+  const toggleUrl = isLoginMode ? "?mode=signup" : "?mode=login";
+  const icon = isLoginMode ? <FaLock /> : <FaUserPlus />;
+
   return (
     <form method="post" className="form" id="auth-form">
-      <div className="icon-img">
-        <FaLock />
-      </div>
+      <div className="icon-img">{icon}</div>
       <p>
         <label htmlFor="email">Email Address</label>
         <input type="email" id="email" name="email" required />
@@ -15,8 +23,8 @@ function AuthForm() {
         <input type="password" id="password" name="password" minLength={7} />
       </p>
       <div className="form-actions">
-        <button>Login</button>
-        <a href="/auth">Log in with existing user</a>
+        <button>{submitBtnCaption}</button>
+        <Link to={toggleUrl}>{toggleBtnCaption}</Link>
       </div>
     </form>
   );
