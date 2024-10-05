cbe189db3f7d8a25bc2ab6d58c384ca510f0d974
Andrii Vakhotskyi <andrey.vahotskiy@devit.group>
2024-10-05 11:05:03 +0300

    Add meta tags and font styles for the root page

--- app/root.jsx
+++ app/root.jsx
@@ -10,7 +10,11 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="description" content="Saldo Finance"></meta>
         <Meta />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;700&display=swap" rel="stylesheet" />
         <Links />
       </head>
       <body>
@@ -28,3 +32,9 @@ export const links = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
   { rel: "stylesheet", href: sharedStyles },
 ];
+
+export const meta = () => {
+  return {
+    title: "Saldo Finance",
+  };
+};
