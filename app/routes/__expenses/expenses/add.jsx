286b4887335767b64ae52cd9f3003fa3ffee0bc0
Andrii Vakhotskyi <andrey.vahotskiy@devit.group>
2024-10-06 12:29:27 +0300

    refactor: add validation errors to ExpenseForm

--- app/routes/__expenses/expenses/add.jsx
+++ app/routes/__expenses/expenses/add.jsx
@@ -1,8 +1,12 @@
+import { redirect } from "@remix-run/node";
 import { useNavigate } from "@remix-run/react";
 
 import ExpenseForm from "~/components/expenses/ExpenseForm";
 
 import Modal from "~/components/util/Modal";
+
+import { addExpenses } from "~/data/expenses.server";
+import { validateExpenseInput } from "~/data/validation.server";
 
 export default function AddExpensesPage() {
   const navigate = useNavigate();
@@ -16,4 +20,18 @@ export default function AddExpensesPage() {
       <ExpenseForm />
     </Modal>
   );
+}
+
+export async function action({ request }) {
+  const formData = await request.formData();
+  const expenseData = Object.fromEntries(formData);
+
+  try {
+    validateExpenseInput(expenseData);
+  } catch (error) {
+    return error;
+  }
+
+  await addExpenses(expenseData);
+  return redirect("/expenses");
 }
