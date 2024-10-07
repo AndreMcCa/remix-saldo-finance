0c35472e0143210a591950a5c5917fc9dba50572
Andrii Vakhotskyi <andrey.vahotskiy@devit.group>
2024-10-07 22:43:40 +0300

    Adding Visual Submission Feedback

--- app/components/expenses/ExpenseForm.jsx
+++ app/components/expenses/ExpenseForm.jsx
@@ -1,24 +1,26 @@
-import { Link, useActionData, useSubmit } from "@remix-run/react";
+import { Form, Link, useActionData, useNavigation /* useSubmit */ } from "@remix-run/react";
 
 function ExpenseForm() {
   const validationErrors = useActionData();
-  const submit = useSubmit();
+  const navigation = useNavigation();
+  // const submit = useSubmit();
 
+  const isSubmitting = navigation.state !== "idle";
   const today = new Date().toISOString().slice(0, 10); // yields something like 2023-09-10
 
-  const submitHandler = (event) => {
-    event.preventDefault();
-    // perform your own validation
-    // ...
+  // const submitHandler = (event) => {
+  //   event.preventDefault();
+  //   // perform your own validation
+  //   // ...
 
-    submit(event.currentTarget, {
-      // action: "/expenses/add",
-      method: "post",
-    });
-  };
+  //   submit(event.currentTarget, {
+  //     // action: "/expenses/add",
+  //     method: "post",
+  //   });
+  // };
 
   return (
-    <form method="post" className="form" id="expense-form" onSubmit={submitHandler}>
+    <Form method="post" className="form" id="expense-form" /* onSubmit={submitHandler} */>
       <p>
         <label htmlFor="title">Expense Title</label>
         <input type="text" id="title" name="title" required maxLength={30} />
@@ -42,10 +44,10 @@ function ExpenseForm() {
         </ul>
       )}
       <div className="form-actions">
-        <button>Save Expense</button>
+        <button disabled={isSubmitting}>{isSubmitting ? "Saving..." : "Save Expense"}</button>
         <Link to="..">Cancel</Link>
       </div>
-    </form>
+    </Form>
   );
 }
 
